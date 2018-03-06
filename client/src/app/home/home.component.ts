9140d8130e046b53fe916afdf45b4e045053a5b1
rocherno <dejon076@morris.umn.edu>
2018-03-05 22:41:30 -0600

    issue#17: Add buttons to home page. Remove buttons from trackers tab. Fix some code in tracker-list-component.
    -Roch

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,25 @@
 import {Component} from '@angular/core';
+import {TrackerListService} from "../trackers/tracker-list.service";
+import {Tracker} from "../trackers/tracker";
 
 @Component({
+    selector: 'app-home-list-component',
     templateUrl: 'home.component.html'
 })
 export class HomeComponent {
-    public text: string;
 
-    constructor() {
-        this.text = 'Mongo lab';
+    constructor(public trackerListService: TrackerListService) {
+
+    }
+
+    public addEmotion(newEmoji: string): void {
+        const newTracker: Tracker = {_id: '', emoji: newEmoji, date: ''};
+        this.trackerListService.addNewEmoji(newTracker).subscribe(
+            trackers => {
+            },
+            err => {
+                console.log(err);
+            }
+        );
     }
 }
