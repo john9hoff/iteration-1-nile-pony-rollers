1a18f0a8f0d5dbc5d21100c283bba8b99828c69b
rocherno <dejon076@morris.umn.edu>
2018-03-07 03:39:59 -0600

    Finish merge.
    -Roch

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
 import {TrackerListComponent} from './trackers/tracker-list.component';
+import {JournalListComponent} from './journals/journal-list.component';
 
 // Route Configuration
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'users', component: UserListComponent},
-    {path: 'trackers', component: TrackerListComponent}
+    {path: 'trackers', component: TrackerListComponent},
+    {path: 'journals', component: JournalListComponent}
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(routes);
