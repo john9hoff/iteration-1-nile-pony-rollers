055fd3c3325f99424450778449070be4b159e414
Francisco Montanez <monta215@morris.umn.edu>
2018-03-22 21:40:03 -0500

    fix code to display graph
    we fixed the in order to display the graph. It currently has dummy values
    while we figure out how to use our own. #11

--- client/src/app/reports/report-chart.component.ts
+++ client/src/app/reports/report-chart.component.ts
@@ -1,54 +1,75 @@
-import {Component} from '@angular/core';
-import {Chart} from 'chart.js'
+import {AfterViewInit, Component} from '@angular/core';
+/* import {Chart} from 'chart.js'; */
+import * as Chart from 'chart.js';
 
 @Component({
     selector: 'report-chart-component',
     templateUrl: 'report-chart.component.html',
     styleUrls: ['./report-chart.component.css'],
 })
-export class ReportChartComponent {
+export class ReportChartComponent implements AfterViewInit {
     constructor() {
-
     }
 
-    /* sample code for trying to get charts to work*/
-    ctx: HTMLElement = document.getElementById("myChart");
+    canvas: any;
+    ctx: any;
 
-    myChart: Chart = new Chart(this.ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+    ngAfterViewInit() {
+        this.canvas = document.getElementById('myChart');
+        this.ctx = this.canvas.getContext('2d');
+
+        //global options
+
+        Chart.defaults.global.defaultFontFamily = 'lato';
+        Chart.defaults.global.defaultFontColor = 'black';
+        //Chart.defaults.global.defaultFontSize = 18;
+
+        let myChart = new Chart(this.ctx, {
+            type: 'horizontalBar',
+
+            data: {
+                labels: ['Radiant', 'Happy', 'Meh', 'Down', 'Sad'],
+
+                datasets: [{
+                    label: 'Total Emotions Logged',
+
+                    data: [5, 7, 2, 6, 9],
+
+                    backgroundColor: ['blue', 'green', 'yellow', 'orange', 'red'],
+
+                    borderWidth: 1,
+
+                    borderColor: 'black',
+
+                    hoverBorderWidth: 3,
                 }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                        }
+                    }]
+                },
+                title: {
+                    display: true,
+                    text: 'Emotion Graph',
+                    fontSize: 30,
+                },
+                legend: {
+                    position: 'bottom',
+                    display: false,
+                },
+                layout: {
+                    // stuff can go here
+                },
+                toolTips: {
+                    enabled: true,
+                },
+                responsive: false,
+                display: true
             }
-        }
-    });
-
+        });
+    }
 }
