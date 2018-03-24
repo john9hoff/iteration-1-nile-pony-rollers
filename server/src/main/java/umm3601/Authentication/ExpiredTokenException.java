e93bb425a4f0ccf80bb2166093fff9dc6e8c954f
rocherno <dejon076@morris.umn.edu>
2018-03-23 19:28:35 -0500

    issue 19: Create ExpiredTokenException file
    -Roch

---
+++ server/src/main/java/umm3601/Authentication/ExpiredTokenException.java
@@ -0,0 +1,13 @@
+package umm3601.Authentication;
+
+
+/**
+ * This exception should be thrown to indicate that someone
+ * tried to use a token that has a limited lifespan and is
+ * past that limit.
+ */
+public class ExpiredTokenException extends Exception {
+    public ExpiredTokenException(String message) {
+        super(message);
+    }
+}
