5e317b0742b2a1ca094bed523e290ddd256451bf
rocherno <dejon076@morris.umn.edu>
2018-03-26 12:05:07 -0500

    issue #10: Add function deleteGoals in GoalRequestHandler
    -Roch

--- server/src/main/java/umm3601/goal/GoalRequestHandler.java
+++ server/src/main/java/umm3601/goal/GoalRequestHandler.java
@@ -146,4 +146,43 @@ public class GoalRequestHandler {
         }
     }
 
+    public String deleteGoal(Request req, Response res)
+    {
+
+        res.type("application/json");
+        Object o = JSON.parse(req.body());
+        try {
+            // if the object that is the JSON representation of the request body's class is the class BasicDBObject
+            // then try to add the item with itemController's editGoal method
+            if(o.getClass().equals(BasicDBObject.class)) {
+                try {
+                    BasicDBObject dbO = (BasicDBObject) o;
+
+                    String id = dbO.getString("_id");
+                    String purpose = dbO.getString("purpose");
+                    String category = dbO.getString("category");
+                    String name = dbO.getString("name");
+                    Boolean status = dbO.getBoolean("status");
+
+                    System.err.println("Deleting goal [purpose=" + purpose + ", category=" + category + ", name=" + name + ", status=" + status + ']');
+                    return goalController.deleteGoal(id, purpose, category, name, status).toString();
+                } catch (NullPointerException e) {
+                    System.err.println("A value was malformed or omitted, new item request failed.");
+                    return null;
+                }
+
+            }
+            else
+            {
+                System.err.println("Expected BasicDBObject, received " + o.getClass());
+                return null;
+            }
+        }
+        catch(RuntimeException ree)
+        {
+            ree.printStackTrace();
+            return null;
+        }
+    }
+
 }
