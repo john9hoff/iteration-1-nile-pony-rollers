b9d180b3925ae920be916e97ed46a6ce0cf18480
Francisco Montanez <monta215@morris.umn.edu>
2018-03-26 23:11:06 -0500

    add e2e testing for report-chart
    currently the second test does not work but we are working to fix it

--- client/e2e/report-chart.e2e-spec.ts
+++ client/e2e/report-chart.e2e-spec.ts
@@ -1,5 +1,20 @@
 import {ReportChartPage} from "./report-chart.po";
+import {browser, protractor} from "protractor";
 
+const origFn = browser.driver.controlFlow().execute;
+
+browser.driver.controlFlow().execute = function () {
+    let args = arguments;
+
+    // queue 100ms wait between test
+    // This delay is only put here so that you can watch the browser do its thing.
+    // If you're tired of it taking long you can remove this call
+    origFn.call(browser.driver.controlFlow(), function () {
+        return protractor.promise.delayed(100);
+    });
+
+    return origFn.apply(browser.driver.controlFlow(), args);
+};
 
 describe('Report Page', () => {
     let page: ReportChartPage;
@@ -13,5 +28,7 @@ describe('Report Page', () => {
         ReportChartPage.navigateTo();
     });
 
-
+    it('should get and highlight chart', () => {
+       expect(page.getChart()).toEqual('myChart');
+    });
 });
