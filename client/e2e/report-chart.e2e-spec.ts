0aa8e97f1bfe9d4f2a4e4603e3187c02e70e760f
Francisco Montanez <monta215@morris.umn.edu>
2018-03-27 15:15:04 -0500

    add e2e testing for report-chart #22
    add test to check to see if chart is present
    
    remove unnecessary code

--- client/e2e/report-chart.e2e-spec.ts
+++ client/e2e/report-chart.e2e-spec.ts
@@ -1,20 +1,5 @@
 import {ReportChartPage} from "./report-chart.po";
-import {browser, protractor} from "protractor";
-
-const origFn = browser.driver.controlFlow().execute;
-
-browser.driver.controlFlow().execute = function () {
-    let args = arguments;
-
-    // queue 100ms wait between test
-    // This delay is only put here so that you can watch the browser do its thing.
-    // If you're tired of it taking long you can remove this call
-    origFn.call(browser.driver.controlFlow(), function () {
-        return protractor.promise.delayed(100);
-    });
-
-    return origFn.apply(browser.driver.controlFlow(), args);
-};
+import {element, by} from "protractor";
 
 describe('Report Page', () => {
     let page: ReportChartPage;
@@ -28,7 +13,8 @@ describe('Report Page', () => {
         ReportChartPage.navigateTo();
     });
 
-    it('should get and highlight chart', () => {
-       expect(page.getChart()).toEqual('myChart');
+    // checks chart is present
+    it('chart is present', () => {
+       expect(element(by.id('myChart')).isPresent()).toBeTruthy();
     });
 });
--- client/e2e/report-chart.po.ts
+++ client/e2e/report-chart.po.ts
@@ -1,28 +1,8 @@
-import {browser, promise, by, element} from 'protractor';
+import {browser, promise} from 'protractor';
 
 export class ReportChartPage {
 
     static navigateTo(): promise.Promise<any> {
         return browser.get('/reports');
     };
-
-    // http://www.assertselenium.com/protractor/highlight-elements-during-your-protractor-test-run/
-    highlightElement(byObject) {
-        function setStyle(element, style) {
-            const previous = element.getAttribute('style');
-            element.setAttribute('style', style);
-            setTimeout(() => {
-                element.setAttribute('style', previous);
-            }, 200);
-            return 'highlighted';
-        }
-        return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
-    };
-
-    static getChart() {
-        const chart = element(by.id('myChart'));
-        //this.highlightElement(by.id('myChart'));
-        return chart;
-    }
-
 }
